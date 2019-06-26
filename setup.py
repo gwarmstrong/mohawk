a6211a5f58ed1e69a7d61c55ee214f3c5cffe3c7
gwarmstrong <garmstrong@colgate.edu>
2019-06-26 09:40:20 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mohawk',
+    version='0.1dev',
+    packages=find_packages(),
+    author="George Armstrong",
+    license='BSD-3-Clause',
+    author_email="garmstro@eng.ucsd.edu",
+    url="https://github.com/gwarmstrong/mohawk",
+    description="Taxonomic Classification and Metagenomic Experiment simulator"
+)
