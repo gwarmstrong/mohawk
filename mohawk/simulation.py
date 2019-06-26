31e55ae61ac84bf2c4a9c5f53cb7c8375db86654
gwarmstrong <garmstrong@colgate.edu>
2019-06-26 16:59:13 -0700

    added id_to_lineage for class determination

--- mohawk/simulation.py
+++ mohawk/simulation.py
@@ -5,7 +5,7 @@ import skbio
 from pkg_resources import resource_stream
 from typing import List, Optional
 
-from mohawk.utils import full_fna_path
+from mohawk.utils import full_fna_path, _get_taxonomy
 from mohawk._format import sample_from_contig_set
 
 representative_genomes_lineage = resource_stream(
@@ -51,15 +51,56 @@ def simulate_from_genomes(id_list: List[str],
     all_reads = []
     for idx, (id_, depth) in enumerate(zip(id_list, id_depths)):
         if depth > 0:
-            sequence_file = full_fna_path(sequence_directory, id_, lineage_info)
+            sequence_file = full_fna_path(sequence_directory, id_,
+                                          lineage_info)
             sequences = list(skbio.io.read(sequence_file, format='fasta'))
-            reads = sample_from_contig_set(sequences, depth, length, random_func)
+            reads = sample_from_contig_set(sequences, depth, length,
+                                           random_func)
             all_reads.append(reads)
 
     reads = np.vstack(all_reads)
 
-    classes = [id_ for id_, depth in zip(id_list, id_depths) for _ in
-               range(depth)]
+    ids = [id_ for id_, depth in zip(id_list, id_depths) for _ in
+           range(depth)]
 
-    return reads, classes
+    return reads, ids
+
+
+def id_to_lineage(ids: List[str],
+                  level: str,
+                  channel: Optional[str] = 'representative') -> List[str]:
+    """
+
+    Parameters
+    ----------
+    ids
+        List of assembly accession ids
+    level
+        Taxonomic level to use for class labels
+        Options: ['superkingdom', 'phylum', 'class', 'order', 'family',
+          'genus', 'species']
+    channel
+        The set of assembly accessions that should be checked for the
+          ids in `ids`
+
+    Returns
+    -------
+    List[str]
+        Each entry is the taxonomic classification at `level` for
+          each id
+
+    """
+
+    if channel == 'representative':
+        lineage_info = pd.read_csv(representative_genomes_lineage,
+                                   sep='\t', index_col=0)
+    elif channel == 'complete':
+        lineage_info = pd.read_csv(complete_genomes_lineage, sep='\t',
+                                   index_col=0)
+    else:
+        raise ValueError("Invalid choice for `channel`. Options are "
+                         "'representative' and 'complete'.")
+
+    return [_get_taxonomy(id_, lineage_info, level) for id_ in ids]
+
 
