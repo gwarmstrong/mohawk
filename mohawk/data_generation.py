af43b0339f81e9e908983b8446d3dab95dc7cb6a
gwarmstrong <garmstrong@colgate.edu>
2019-06-25 17:15:34 -0700

    initial data_generation scaffold

---
+++ mohawk/data_generation.py
@@ -0,0 +1,235 @@
+import os
+import gzip
+import shutil
+import numpy as np
+import pandas as pd
+from ftplib import FTP
+from typing import List, Tuple, Optional, Dict
+
+ranks = ['infraspecific_name', 'species', 'genus', 'family', 'order', 'class',
+         'phylum', 'superkingdom']
+
+representative_genomes_file = 'refseq_representative_genomes_taxonomy_slim.txt'
+
+complete_genomes_file = 'refseq_complete_genomes_taxonomy_slim.txt'
+
+
+def _ncbi_ftp_downloader(id_list: List[str],
+                         genomes_metadata: pd.DataFrame,
+                         genomes_directory: str) -> bool:
+
+    ftp = FTP('ftp.ncbi.nih.gov')
+    ftp.login(user='anonymous', passwd='example@net.com')
+    for id_ in id_list:
+        # TODO change into a getter that is passed in
+        def ftp_path(id_): return genomes_metadata['ftp_path'].loc[id_] #
+        abspath = ftp_path(id_)
+        ftp_dir = get_ftp_dir(abspath)
+        ftp.cwd(ftp_dir)
+        filename = get_fna_name(ftp_dir)
+        local_dir = os.path.join(genomes_directory, id_, filename)
+        ftp.retrbinary("RETR " + filename, open(local_dir, 'wb').write)
+
+    return True
+
+
+def _gunzip(gz_file: str, gunzipped_file: str) -> bool:
+    # see 'https://stackoverflow.com/questions/48466421/python-how-to-'
+    #     'decompress-a-gzip-file-to-an-uncompressed-file-on-disk'
+    with gzip.open(gz_file, 'r') as f_in, \
+            open(gunzipped_file, 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+    return True
+
+
+def _file_gunzipper(id_list: List[str],
+                    genomes_metadata: pd.DataFrame,
+                    genomes_directory: str) -> bool:
+
+    for id_ in id_list:
+        # TODO change into a getter that is passed in
+        def ftp_path(id_): return genomes_metadata['ftp_path'].loc[id_]
+        abspath = ftp_path(id_)
+        ftp_dir = get_ftp_dir(abspath)
+        filename = get_fna_name(ftp_dir)
+        fna_gz_filename = os.path.join(genomes_directory, id_, filename)
+        fna_filename = gz_stripper(fna_gz_filename)
+        _gunzip(fna_gz_filename, fna_filename)
+
+    return True
+
+
+def gz_stripper(filename: str) -> str:
+    if filename[-3:] == '.gz':
+        return filename[:-3]
+    else:
+        return filename
+
+
+def get_fna_name(ftp_dir: str) -> str:
+
+    return ftp_dir.split('/')[-1] + '_genomic.fna.gz'
+
+
+def get_ftp_dir(abspath: str) -> str:
+    """
+    Returns path within ftp site
+
+    Parameters
+    ----------
+    abspath
+        The full ftp url
+
+    Returns
+    -------
+    str
+        The relative path within the ftp site
+
+    Examples
+    --------
+    >>> abspath = 'ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/900/128/'\
+                  '725/GCF_900128725.1_BCifornacula_v1.0'
+    >>> get_ftp_dir(abspath)
+    'genomes/all/GCF/900/128/725/GCF_900128725.1_BCifornacula_v1.0'
+
+    """
+    return '/' + '/'.join(abspath.split('/')[3:])
+
+
+def _get_ids_not_downloaded(id_list: List[str],
+                            genomes_metadata: pd.DataFrame,
+                            genomes_directory: Optional[str],
+                            fna_only: Optional[bool] = False) -> List[str]:
+
+    # if fna_only, we are checking for ids to gunzip
+    # else, we are checking to see if we need to download the .gz file
+    # results in the following truth table:
+    # to_download | .fna exists | .fna.gz exists | fna_only
+    #       T     |      F      |        F       |    F
+    #       F     |      F      |        T       |    F
+    #       F     |      T      |        T       |    F
+    #       F     |      T      |        F       |    F
+    #  err<-F     |      F      |        F       |    T
+    #       T     |      F      |        T       |    T
+    #       F     |      T      |        T       |    T
+    #       F     |      T      |        F       |    T
+    # throws value error if a requested id does not have a .fna or .fna.gz
+    # and is asked whether it should gunzip the .fna.gz, likely indicates a
+    # failed download earlier on
+
+    if genomes_directory is None:
+        genomes_directory = os.path.curdir()
+
+    def ftp_path(id_): return genomes_metadata['ftp_path'].loc[id_]
+
+    ids_to_download = []
+    for id_ in id_list:
+        expected_local_dir = os.path.join(genomes_directory,
+                                          id_)
+        fna_gz_name = get_fna_name(ftp_path(id_))
+        fna_name = gz_stripper(fna_gz_name)
+        try:
+            existing_files = set(os.listdir(expected_local_dir))
+            fna_present = fna_name in existing_files
+            fna_gz_present = fna_gz_name in existing_files
+            if not fna_only:
+                if not fna_present and not fna_gz_present:
+                    ids_to_download.append(id_)
+            else:
+                if not fna_present and fna_gz_present:
+                    ids_to_download.append(id_)
+                elif not fna_present:
+                    raise ValueError('Cannot gunzip when .gz file is not '
+                                     'present, ID: {}'.format(id_))
+
+        # if the directory does not exist, then create a directory for it
+        # and add the id to the download list
+        # TODO: more specific error catching?
+        except FileNotFoundError:
+            os.makedirs(expected_local_dir, exist_ok=True)
+            ids_to_download.append(id_)
+
+    return ids_to_download
+
+
+# TODO this name
+def _assure_all_data(id_list: List[str],
+                     genomes_metadata: pd.DataFrame,
+                     genomes_directory: Optional[str]) -> bool:
+
+    if genomes_directory is None:
+        genomes_directory = os.path.curdir()
+
+    # TODO catch a ValueError for the id passed not being in the channel
+    possible_ids = set(genomes_metadata.index)
+    for id_ in id_list:
+        if id_ not in possible_ids:
+            raise ValueError('Invalid assembly accession ID for this '
+                             'channel: {}'.format(id_))
+
+    ids_to_download = _get_ids_not_downloaded(id_list,
+                                              genomes_metadata,
+                                              genomes_directory)
+
+    # download .fna.gz files we do not have (do not need to download if
+    # .fna exists)
+
+    _ncbi_ftp_downloader(ids_to_download, genomes_metadata, genomes_directory)
+
+    # if .fna.gz files are not unzipped, unzip them
+    ids_to_gunzip = _get_ids_not_downloaded(id_list, genomes_metadata,
+                                            genomes_directory, fna_only=True)
+
+    _file_gunzipper(ids_to_gunzip, genomes_metadata, genomes_directory)
+
+    return True
+
+
+# potential for shuffle option ? -> argshuffle
+def _data_generator(genome_ids: List[str],
+                    taxonomic_rank: str,
+                    read_length: int,
+                    reads_per_sample: int,
+                    genomes_metadata: pd.DataFrame,
+                    genomes_directory: Optional[str],
+                    random_seed: Optional[int]) -> Tuple[List[str], List[int]]:
+
+    # for each `genome` in `genome_ids` (found in `input_directory`):
+        # generate `sequences_per_genome` reads of length
+        #   `read_length` from genome
+        # figure out appropriate class for genome (based on `rank`)
+
+    pass
+
+
+def data_generator(genome_ids: List[str],
+                   taxonomic_rank: str,
+                   read_length: int,
+                   reads_per_sample: int,
+                   distribution: Optional[Dict[str, float]] = None,
+                   genomes_directory: Optional[str] = None,
+                   sample_directory: Optional[str] = None,
+                   random_seed: Optional[int] = None,
+                   channel: Optional[str] = 'representative') -> str:
+
+    # option handling
+
+    if channel == 'representative':
+        genomes_metadata = pd.read_csv(representative_genomes_file,
+                                       sep='\t', index_col=0)
+    elif channel == 'complete':
+        genomes_metadata = pd.read_csv(complete_genomes_file, sep='\t',
+                                       index_col=0)
+    else:
+        raise ValueError("Invalid choice for `channel`. Options are "
+                         "'representative' and 'complete'.")
+
+    # make sure all genomes are downloaded (download if not)
+    _assure_all_data(genome_ids, genomes_metadata, genomes_directory)
+
+    # use _data_generator to take each genome id and generate the reads
+
+    # write out fastq files and class file
+
+    pass
