44bad37c77ba1a7f5c6ebc96a781f90d75bc4b1a
gwarmstrong <garmstrong@colgate.edu>
2019-06-26 22:04:54 -0700

    first dummy model

---
+++ mohawk/models.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+
+class DummyModel(nn.Module):
+
+    def summarize(self, directory):
+        # TODO train and test accuracies
+        # maybe save predictions?
+        # definitely save trained model
+
+        pass
