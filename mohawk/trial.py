5c1bdfe996f3afff9898a8ee2e93c29087bf49df
gwarmstrong <garmstrong@colgate.edu>
2019-06-28 17:41:53 -0700

    big trial

--- mohawk/trial.py
+++ mohawk/trial.py
@@ -4,14 +4,37 @@ import torch
 from mohawk.trainer import trainer
 from mohawk.models import BaseModel, SmallConvNet
 
-trial_ids = [
-             'GCF_000011545.1',  # Burkholderia pseudomallei K96243
-             'GCF_001999985.1',  # Helicobacter bilis
-             'GCF_000953655.1',  # Legionella hackeliae
-             'GCF_000590475.1',  # Pseudomonas stutzeri
-             'GCF_002209165.2',  # Staphylococcus sciuri
-             'GCF_001558415.2',  # Vibrio fluvialis
-             ]
+# trial_ids = [
+#              'GCF_000011545.1',  # Burkholderia pseudomallei K96243
+#              'GCF_001999985.1',  # Helicobacter bilis
+#              'GCF_000953655.1',  # Legionella hackeliae
+#              'GCF_000590475.1',  # Pseudomonas stutzeri
+#              'GCF_002209165.2',  # Staphylococcus sciuri
+#              'GCF_001558415.2',  # Vibrio fluvialis
+#              ]
+# all ids not in validation
+trial_ids = ['GCF_000006745.1', 'GCF_000006765.1', 'GCF_000007565.2',
+             'GCF_000007645.1', 'GCF_000007805.1', 'GCF_000008485.1',
+             'GCF_000008525.1', 'GCF_000009865.1', 'GCF_000010125.1',
+             'GCF_000011545.1', 'GCF_000011705.1', 'GCF_000012245.1',
+             'GCF_000016565.1', 'GCF_000017705.1', 'GCF_000025085.1',
+             'GCF_000091465.1', 'GCF_000091785.1', 'GCF_000091985.1',
+             'GCF_000185885.1', 'GCF_000196095.1', 'GCF_000200595.1',
+             'GCF_000213805.1', 'GCF_000217675.1', 'GCF_000219605.1',
+             'GCF_000259255.1', 'GCF_000259275.1', 'GCF_000332735.1',
+             'GCF_000349975.1', 'GCF_000354175.2', 'GCF_000397205.1',
+             'GCF_000412695.1', 'GCF_000590475.1', 'GCF_000706685.1',
+             'GCF_000761155.1', 'GCF_000763535.1', 'GCF_000772105.1',
+             'GCF_000801275.2', 'GCF_000816085.1', 'GCF_000818015.1',
+             'GCF_000953135.1', 'GCF_000953655.1', 'GCF_000959245.1',
+             'GCF_001028645.1', 'GCF_001411495.1', 'GCF_001432245.1',
+             'GCF_001460635.1', 'GCF_001534745.1', 'GCF_001558415.2',
+             'GCF_001559115.2', 'GCF_001597285.1', 'GCF_001602095.1',
+             'GCF_001618885.1', 'GCF_001654435.1', 'GCF_001677275.1',
+             'GCF_001685465.1', 'GCF_001902315.1', 'GCF_001913135.1',
+             'GCF_001999985.1', 'GCF_002101335.1', 'GCF_002196515.1',
+             'GCF_002208805.2', 'GCF_002209165.2', 'GCF_002215135.1',
+             'GCF_002240035.1']
 validation_ids = [
                   'GCF_000959725.1',  # Burkholderia gladioli
                   'GCF_000007905.1',  # Helicobacter hepaticus
@@ -20,16 +43,20 @@ validation_ids = [
                   'GCF_000013425.1',  # Staphylococcus aureus
                   'GCF_001547935.1',  # Vibrio tritonius
                   ]
+
+# try another thing... train on all references for these clades that are not
+# in validation id's (make sure to exclude strain)
+
 trial_directory = '../data/trial_download'
 distribution = [1/6]*6
-total_reads = 200000
+total_reads = 200000 * 15
 length = 150
 train_ratio = 0.8
 seed = 1234
 
 external_validation_params = {
     'external_validation_ids': validation_ids,
-    'n_external_validation_reads': 50000,
+    'n_external_validation_reads': 200000,
     'external_validation_distribution': distribution,
 }
 
