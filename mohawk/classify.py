d79da539807d1237596d24a50156a5f5448f72a4
gwarmstrong <garmstrong@colgate.edu>
2019-07-16 16:31:30 -0700

    finsh classify

--- mohawk/classify.py
+++ mohawk/classify.py
@@ -6,7 +6,9 @@ from mohawk.models import BaseModel
 from typing import Union, Optional
 import skbio
 import numpy as np
+import pandas as pd
 from mohawk._format import encode_sequence
+from mohawk.trainer import prepare_dataloader
 
 
 def classify(model: Union[BaseModel, str],  # assumes a trained model,
@@ -14,7 +16,6 @@ def classify(model: Union[BaseModel, str],  # assumes a trained model,
              length: int,
              data_path: str,  # path to a FASTA file
              pad_value: Optional[int] = 0,
-             level: Optional[str] = 'genus',  # TODO might not need
              batch_size: Optional[int] = 1,
              format_kwargs: Optional[dict] = None,
              format_: Optional[str] = 'fasta',  # input to skbio.io.read
@@ -25,30 +26,67 @@ def classify(model: Union[BaseModel, str],  # assumes a trained model,
     if output_directory is None:
         output_directory = os.path.curdir
 
+    if format_kwargs is None:
+        format_kwargs = dict()
+
     if isinstance(model, str):
         model = load_trained_model(model)
 
+    model.double()
+
+    print("Read sequences...")
     sequences = list(skbio.io.read(data_path,
                                    format=format_,
                                    **format_kwargs))
+    print("Done.")
 
+    print("Encode sequences...")
     encoded = [(encode_sequence(seq), seq.metadata['id']) for seq in sequences]
+    print("Done.")
 
     # TODO support to split each read in encoded into `length`-mers and keep
     #  some form of identifier for each of the sequence so they can be
     #  mapped back
     # TODO make function and move to _format.py
+    print("Ensure lengths...")
     encoded_sequences, ids = ensure_lengths(encoded,
                                             length,
                                             pad_value=pad_value)
-
-    encoded_sequences = np.array(encoded_sequences)
-
+    print("Done.")
+    print("Data size: {}".format(encoded_sequences.shape))
+    print("Prepare dataloader...")
     # TODO turn `encoded` into a dataset/dataloader
+    dataloader = prepare_dataloader(encoded_sequences,
+                                    ids=ids,
+                                    batch_size=batch_size)
+    print("Done.")
 
+    print("Predicting...")
     # TODO predict on `encoded` dataset
+    all_ids = []
+    all_class_predictions = []
+    for index_epoch, data in enumerate(dataloader):
+        print("it: {} out of {}".format(index_epoch, len(dataloader) - 1))
+        epoch_ids = data['id']
+        reads = data['read']
+        # TODO here would be the place to put to device
+        softmax_predictions = model(reads)
+        predictions = softmax_predictions.argmax(1).numpy()
+        all_ids.append(epoch_ids)
+        all_class_predictions.append(predictions)
 
-    # TODO write out results to output_directory
+    all_ids = np.concatenate(all_ids)
+    all_class_predictions = np.concatenate(all_class_predictions)
+    print("Done.")
+
+    transformed_predictions = model.class_encoder.inverse_transform(
+        all_class_predictions)
+
+    results = pd.DataFrame({'ids': all_ids,
+                            'predictions': transformed_predictions})
+
+    output_name = os.path.join(output_directory, 'prediction_results.txt')
+    results.to_csv(output_name, sep='\t', index=False)
 
 
 # TODO maybe rename `encoded`
@@ -59,16 +97,17 @@ def ensure_lengths(encoded, length, pad_value=0):
         if len(sequence) < length:
             # if sequence is not long enough, add padding
             this_sequence = sequence.copy()
-            while len(this_sequence) < length:
-                this_sequence.append(pad_value)
+            pad_length = length - len(sequence)
+            this_sequence = np.append(this_sequence, [pad_value] * pad_length)
             all_sequences.append(this_sequence)
-            all_ids.append(id_)
+            all_ids.append(id_ + '__idx_0')
         else:
             for start_index in range(len(sequence) - length + 1):
                 this_sequence = sequence[start_index: start_index + length]
                 all_sequences.append(this_sequence)
-                all_ids.append(id_)
+                all_ids.append(id_ + '__idx_{}'.format(start_index))
 
+    all_sequences = np.vstack(all_sequences)
     return all_sequences, all_ids
 
 
@@ -77,6 +116,8 @@ def load_trained_model(filepath):
     checkpoint = torch.load(filepath)
     model = checkpoint['model']
     model.load_state_dict(checkpoint['model_state_dict'])
+
+    model.class_encoder = checkpoint.get('class_encoder', None)
 
     model.eval()
 
