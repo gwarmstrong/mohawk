3129809ca7590dd88db304f2a71af637f708a5b6
George Armstrong <garmstrong@colgate.edu>
2019-10-18 16:15:44 -0700

    add tests for labels being passed to trainer

--- mohawk/tests/test_trainer.py
+++ mohawk/tests/test_trainer.py
@@ -1,6 +1,6 @@
 from mohawk.tests import testing
 from mohawk.trainer import trainer
-from mohawk.models import ConvNetAvg
+from mohawk.models import ConvNetAvg, ConvNetAvg2
 
 
 class TestTrainer(testing.MohawkTestCase):
@@ -9,11 +9,16 @@ class TestTrainer(testing.MohawkTestCase):
 
     def setUp(self) -> None:
         super(TestTrainer, self).setUp()
-        self.model = ConvNetAvg
-        self.distribution = [0.5, 0.5]
+        self.distribution = [0.5, 0.3, 0.2]
         self.id_list = [
                         'GCF_001999985.1',  # Helicobacter bilis
                         'GCF_002209165.2',  # Staphylococcus sciuri
+                        'GCF_000953655.1',  # Legionella hackeliae
+        ]
+        self.classes = [
+                        'class1',
+                        'class2'
+                        'class2'
                         ]
         self.total_reads = 200
         self.length = 150
@@ -26,7 +31,25 @@ class TestTrainer(testing.MohawkTestCase):
                         'summarize': True,
                         'learning_rate': 0.001,
                         'log_dir': self.create_data_path('model_log_1')}
-        trainer(self.model, self.distribution, self.total_reads,
-                self.length, self.train_ratio, self.id_list,
-                data_directory=self.get_data_path('sample_genomes'),
-                train_kwargs=train_kwargs)
+        mod = trainer(ConvNetAvg, self.distribution, self.total_reads,
+                      self.length,
+                      self.train_ratio, self.id_list,
+                      data_directory=self.get_data_path('sample_genomes'),
+                      train_kwargs=train_kwargs)
+
+        self.assertEqual(mod.n_classes, 3)
+
+    def test_trainer_runs_with_classes(self):
+        train_kwargs = {'gpu': False,
+                        'summary_interval': 1,
+                        'epochs': 10,
+                        'summarize': True,
+                        'learning_rate': 0.001,
+                        'log_dir': self.create_data_path('model_log_2')}
+        mod = trainer(ConvNetAvg2, self.distribution, self.total_reads,
+                      self.length,
+                      self.train_ratio, self.id_list, class_list=self.classes,
+                      data_directory=self.get_data_path('sample_genomes'),
+                      train_kwargs=train_kwargs)
+
+        self.assertEqual(mod.n_classes, 2)
