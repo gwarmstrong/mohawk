0aad04bf0d029dedd29d2b7c1a94425ff1b30a99
George Armstrong <garmstrong@colgate.edu>
2019-10-22 19:40:17 -0700

    set up example script run

--- scripts/train_demo.sh
+++ scripts/train_demo.sh
@@ -2,5 +2,6 @@ mohawk train --genome-ids example_input/min-red_0.1__n_4__group_0__genome-ids.tx
     --model-name ConvNetAvg \
     --log-dir example_output \
     --data-dir example_input/genomes \
+    --metadata example_input/wol_supplemental_metadata.tsv \
     --train-ratio 0.8 \
     --gpu True
