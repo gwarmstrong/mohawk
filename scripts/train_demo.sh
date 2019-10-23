41534d199fbb003903751181d56af298ad7de0aa
George Armstrong <garmstrong@colgate.edu>
2019-10-22 18:09:32 -0700

    demo script using mohawk CLI

---
+++ scripts/train_demo.sh
@@ -0,0 +1,6 @@
+mohawk train --genome-ids example_input/min-red_0.1__n_4__group_0__genome-ids.txt \
+    --model-name ConvNetAvg \
+    --log-dir example_output \
+    --data-dir example_input/genomes \
+    --train-ratio 0.8 \
+    --gpu True
